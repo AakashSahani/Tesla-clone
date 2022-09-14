307d13d7a3be51ac92440dce082d978f8a91ba2e
aakash sahani <sahani.aakash120@gmail.com>
2022-09-13 23:41:11 -0400

    Updated text and btn styling

---
+++ script/app.js
@@ -0,0 +1,16 @@
+// $(window).scroll(function () {
+// 	var scrollTop = $(this).scrollTop();
+
+// 	$('.text').css({
+// 		opacity: function () {
+// 			var elementHeight = $(this).height() / 2;
+// 			return (elementHeight - scrollTop) / elementHeight;
+// 		},
+// 	});
+// 	$('.button').css({
+// 		opacity: function () {
+// 			var elementHeight = $(this).height();
+// 			return (elementHeight - scrollTop) / elementHeight;
+// 		},
+// 	});
+// });
